d8d0edc761c1f96dbadab5c73adcdcad7167be3b
JimmyWongIntuit <jimmy_wong@intuit.com>
2017-08-15 22:21:39 -0700

    Removing credentials

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumerKey:     'qyprdAJlp6VHqp0WM0u5UQ8FvYkaUm',
-  consumerSecret:  'etjySyGLigTn5sGtgoxlvvEd1TKemhw5qzmao4X9',
+  consumerKey:     '',
+  consumerSecret:  '',
   token:           '',
   tokenSecret:     '',
   realmId:         '',
@@ -10,5 +10,9 @@ module.exports = {
   // Set useSandbox to false when moving to production. For info, see the following url:
   // https://developer.intuit.com/v2/blog/2014/10/24/intuit-developer-now-offers-quickbooks-sandboxes
   
-  testEmail:       ''  // Use this email address for testing send*Pdf functions
+  testEmail:       '',  // Use this email address for testing send*Pdf functions
+  
+  // //Plot.ly credentials
+  username:        '',
+  api_keys:        ''
 }
